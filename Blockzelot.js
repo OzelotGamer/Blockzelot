a23783d78bc46bfe1f83870f8e63291cd88b7bb8
OzelotGamer <136556115+OzelotGamer@users.noreply.github.com>
2024-06-03 00:08:06 +0200

    Update Blockzelot.js

--- Blockzelot.js
+++ Blockzelot.js
@@ -1,7 +1,9 @@
 var Inter = setInterval(()=>{
 for(i=0;i<document.getElementsByTagName("iframe").length;i++)
   {
+    if(!document.getElementsByTagName("iframe")[i].id.toUpperCase().includes("GAME")){
   document.getElementsByTagName("iframe")[i].remove();
+    }
 document.getElementsByTagName("video")[i].remove();
   }
  }, 100);
